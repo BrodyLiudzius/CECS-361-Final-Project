3a544ffa9a292bdc4fce4a83712d04d814ab7f52
Adan Arevalo <adan.arevalo@student.csulb.edu>
2023-11-17 21:16:20 -0800

    halfadder_tb done

--- OISC.srcs/sim_1/new/HalfAdder_tb.v
+++ OISC.srcs/sim_1/new/HalfAdder_tb.v
@@ -20,12 +20,36 @@ module HalfAdder_tb ();
     
     // PROCEDURES
     integer i;
+    reg expected_sum;
+    reg expected_carry;
+    reg passed;
+
     initial begin
+       passed = 1;
+
         for (i = 0; i < 4; i = i + 1) begin
             {a, b} = i;
             #5;
+            expected_sum = a ^ b;  // XOR for sum
+            expected_carry = a & b;
+
+            if(expected_sum !== sum || expected_carry !== carryOut) begin
+                $display("(", a, ", ", b, ") FAILED.");
+                passed = 0;
+            end
         end
+
+        $display("----------------------");
+        if (passed)
+            $display("All tests passed");
+        else
+            $display("Tests FAILED");
+        $display("----------------------");
+        
         $finish;
     end
-
 endmodule
+
+    
+
+
