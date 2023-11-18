db92780d6e331e48465488e47400941c638cc167
Brody Liudzius <41172959+BrodyLiudzius@users.noreply.github.com>
2023-11-17 17:58:21 -0800

    Created ALU and AddressBook

--- OISC.srcs/sources_1/new/ALU.v
+++ OISC.srcs/sources_1/new/ALU.v
@@ -1,3 +1,5 @@
+// tags: #toDo
+
 
 module ALU #(
         parameter DATA_BUS_WIDTH = 64,
@@ -16,6 +18,69 @@ module ALU #(
     );
 
 
+    `include "AddressBook.vh"
 
+
+    // LOGICAL FUNCTION UNITS
+    Compare #(
+        .DATA_BUS_WIDTH(DATA_BUS_WIDTH),
+        .ADDR_BUS_WIDTH(ADDR_BUS_WIDTH),
+        .INPUT_ADDRESS_1(BASE_ADDRESS + COMPARE_BASE_ADDRESS + 0),
+        .INPUT_ADDRESS_2(BASE_ADDRESS + COMPARE_BASE_ADDRESS + 1),
+        .OUTPUT_ADDRESS(BASE_ADDRESS + COMPARE_BASE_ADDRESS + 0)
+    ) compare (
+        .reset(reset),
+        .clock(clock),
+        .dataBusIn(dataBusIn),
+        .dataBusOut(dataBusOut),
+        .readAddressBus(readAddressBus),
+        .writeAddressBus(writeAddressBus)
+    );
+
+    
+
+    // ARITHMETIC FUNCTION UNITS
+    Adder #(
+        .DATA_BUS_WIDTH(DATA_BUS_WIDTH),
+        .ADDR_BUS_WIDTH(ADDR_BUS_WIDTH),
+        .INPUT_ADDRESS_1(ADDER_BASE_ADDRESS + 0),
+        .INPUT_ADDRESS_2(ADDER_BASE_ADDRESS + 1),
+        .OUTPUT_ADDRESS(ADDER_BASE_ADDRESS + 0)
+    ) adder (
+        .reset(reset),
+        .clock(clock),
+        .dataBusIn(dataBusIn),
+        .dataBusOut(dataBusOut),
+        .readAddressBus(readAddressBus),
+        .writeAddressBus(writeAddressBus)
+    );
+
+    ArithmeticLeftShift #(
+        .DATA_BUS_WIDTH(DATA_BUS_WIDTH),
+        .ADDR_BUS_WIDTH(ADDR_BUS_WIDTH),
+        .INPUT_ADDRESS(A_LSHIFT_BASE_ADDRESS),
+        .OUTPUT_ADDRESS(A_LSHIFT_BASE_ADDRESS)
+    ) arithmeticLeftShift (
+        .reset(reset),
+        .clock(clock),
+        .dataBusIn(dataBusIn),
+        .dataBusOut(dataBusOut),
+        .readAddressBus(readAddressBus),
+        .writeAddressBus(writeAddressBus)
+    );
+
+    ArithmeticRightShift #(
+        .DATA_BUS_WIDTH(DATA_BUS_WIDTH),
+        .ADDR_BUS_WIDTH(ADDR_BUS_WIDTH),
+        .INPUT_ADDRESS(A_RSHIFT_BASE_ADDRESS),
+        .OUTPUT_ADDRESS(A_RSHIFT_BASE_ADDRESS)
+    ) arithmeticRightShift (
+        .reset(reset),
+        .clock(clock),
+        .dataBusIn(dataBusIn),
+        .dataBusOut(dataBusOut),
+        .readAddressBus(readAddressBus),
+        .writeAddressBus(writeAddressBus)
+    );
 
 endmodule
